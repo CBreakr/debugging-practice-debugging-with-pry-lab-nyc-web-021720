b9f109a482fbd2b64e67b22532738ddabeb45ae8
Robert Notwicz <rob.notwicz@gmail.com>
2020-01-18 18:58:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,14 +1,16 @@
-
+#
 def get_user_input
   gets.chomp
 end
 
+#
 def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
 end
 
+#
 def selection(num)
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
@@ -17,6 +19,7 @@ def selection(num)
   end
 end
 
+#
 def runner
   prompt_user
   selection(get_user_input)
